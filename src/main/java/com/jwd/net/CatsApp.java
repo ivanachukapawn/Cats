c3f9e38848ea8ba2d1af97f66d7ee3277248c61a
John Davis <johnwdavis@gmail.com>
2016-07-07 15:37:59 -0400

    version with single home jsp and maven build for Tomcat

--- src/main/java/com/jwd/net/CatsApp.java
+++ src/main/java/com/jwd/net/CatsApp.java
@@ -2,13 +2,14 @@ package com.jwd.net;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
-public class CatsApp
+public class CatsApp extends SpringBootServletInitializer
 {
 
 	public static void main(String[] args)
@@ -17,10 +18,17 @@ public class CatsApp
 	}
 
 	@RequestMapping("/")
-	@ResponseBody
 	String home()
 	{
-		return	"hello you sassy ragamuffin  -  and you are a budabudow";
+		return	"home";
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return	builder.sources(CatsApp.class);
+	}
+	
+	
 
 }
