7ef325d91188b019e51b1d4d57a09bf36690d649
John Davis <johnwdavis@gmail.com>
2016-07-07 17:31:22 -0400

    added access controller

--- src/main/java/com/jwd/net/controllers/AccessController.java
+++ src/main/java/com/jwd/net/controllers/AccessController.java
@@ -1,6 +1,15 @@
 package com.jwd.net.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AccessController
 {
+	@RequestMapping("/humans")
+	String humans()
+	{
+		return	"humans";
+	}
 
 }
