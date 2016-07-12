82cb6668da125e9ba4a0bad4c0458c32b140ae19
John Davis <johnwdavis@gmail.com>
2016-07-12 18:50:54 -0400

    latest dev event displayed with formatted date

--- src/main/java/com/jwd/net/controllers/PageController.java
+++ src/main/java/com/jwd/net/controllers/PageController.java
@@ -2,15 +2,21 @@ package com.jwd.net.controllers;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.jwd.net.model.DevEventUpdate;
+import com.jwd.net.service.DevEventUpdateService;
 
 @Controller
 public class PageController
 {
+	@Autowired
+	private	DevEventUpdateService	devEventUpdateService;
+	
 	@RequestMapping("/")
 	String home()
 	{
@@ -23,14 +29,44 @@ public class PageController
 		return	"app.about";
 	}
 	
-	@RequestMapping("/addnews")
+	@RequestMapping(value="/addnews",method=RequestMethod.GET)
 	ModelAndView addNews(ModelAndView modelAndView)
 	{
 		modelAndView.setViewName("app.addNews");
 		
 		DevEventUpdate	devEventUpdate	=	new	DevEventUpdate();
 		
+		DevEventUpdate	latestDevEvent	=	devEventUpdateService.getLatestDevEvent();
+		
+		Date	latestAdded	=	latestDevEvent.getAdded();
+		String	latestText	=	latestDevEvent.getText();
+		
+		modelAndView.getModel().put("latestAdded", latestAdded);
+		modelAndView.getModel().put("latestText", latestText);
+		
 		modelAndView.getModel().put("devEventUpdate", devEventUpdate);
+		modelAndView.getModel().put("latestDevEvent", latestDevEvent);
+		
+		return	modelAndView;
+	}
+	
+	@RequestMapping(value="/addnews",method=RequestMethod.POST)
+	ModelAndView addNews(ModelAndView modelAndView,DevEventUpdate devEventUpdate)
+	{
+		modelAndView.setViewName("app.addNews");
+		
+		devEventUpdateService.save(devEventUpdate);
+		
+		DevEventUpdate	latestDevEvent	=	devEventUpdateService.getLatestDevEvent();
+		
+		Date	latestAdded	=	latestDevEvent.getAdded();
+		String	latestText	=	latestDevEvent.getText();
+		
+		
+		modelAndView.getModel().put("latestAdded", latestAdded);
+		modelAndView.getModel().put("latestText", latestText);
+		
+		modelAndView.getModel().put("latestDevEvent", latestDevEvent);
 		
 		return	modelAndView;
 	}
--- src/main/webapp/WEB-INF/tiles/addNews.jsp
+++ src/main/webapp/WEB-INF/tiles/addNews.jsp
@@ -1,6 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 	<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+	<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+	<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+	
+	
+	
+	
+	
+	
 
 <div class=row>
 
@@ -17,14 +25,27 @@
 			</div>
 
 			<div class="panel-body">
-				<form>
+			
+				<form:form commandName="devEventUpdate">
 					<div class="form-group">
-						<textarea name="text" rows="10" cols="50"></textarea>
+						<form:textarea path="text" name="text" rows="10" cols="50"></form:textarea>
 					</div>
 				
 					<input type="submit" name="submit" value="add dev event" />
 				
-				</form>	
+				</form:form>	
+			</div>
+
+		</div>
+		
+		<div class="panel panel-default">
+
+			<div class="panel-body">
+			
+				
+				<fmt:formatDate pattern="EEEE MMMM d y 'at ' H mm" value="${latestAdded}"></fmt:formatDate>
+				<c:out value="${latestText}"></c:out>
+				
 			</div>
 
 		</div>
